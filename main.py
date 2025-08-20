8fa30b9e72e4d4ab06b2eae5c67132cb24dc4798
Caress Castanares <cpcastanares@gmail.com>
2025-08-21 01:45:00 +0800

    feat: added cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # from .dependencies import get_query_token, get_token_header
 # from .internal import admin
@@ -23,6 +24,19 @@ app.include_router(event.router)
 app.include_router(rsvp.router)
 app.include_router(comment.router)
 app.include_router(organization.router)
+
+# Configure CORS
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
